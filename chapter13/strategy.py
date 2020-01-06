6dd07fa8e980883d909a72f00fd98081f1ea7e79
E7EE477C11C98D403F318118733EAA5C <E7EE477C11C98D403F318118733EAA5C@i-search.com.cn>
2020-01-06 11:07:50 +0800

    Updated chapter13/strategy.py

--- chapter13/strategy.py
+++ chapter13/strategy.py
@@ -1,4 +1,24 @@
-"""策略模式"""
+"""策略模式
+
+在Python中，我们可以把函数看作是普通的变量，这就简化了策略模式的实现。
+
+问题
+假设我们要实现一个算法来检测在一个字符串中是否所有字符都是唯一的。
+例如，如果输入字符串dream，算法应返回true，因为没有字符是重复的。
+如果输入字符串pizza，算法应返回false，因为字母z出现了两次。
+
+实现
+对字符串进行排序并逐对比较所有字符。
+
+
+优化
+通常，我们想要使用的策略不应该由用户来选择。策略模式的要点是可以透明地
+使用不同的算法。
+
+优化提示
+考虑在一个共用类（例如，AllUnique）中封装两个函数。这样，其他开发人员
+只需要创建一个AllUnique类实例，并执行单个方法，例如test()。
+"""
 
 import time
 
@@ -15,6 +35,10 @@ WARNING = 'too bad, you picked the slow algorithm :('
 
 
 def allUniqueSort(s):
+    """
+    假设这个算法伸缩性不好，对于不超过5个字符串才能工作良好。
+    对于更长的字符串，通过插入一条 sleep 语句来模拟速度减缓。
+    """
     if len(s) > LIMIT:
         print(WARNING)
         time.sleep(SLOW)
@@ -26,6 +50,9 @@ def allUniqueSort(s):
 
 
 def allUniqueSet(s):
+    """
+    假设由于一些奇怪的原因，这个函数检测短字符串性能比 allUniqueSort() 更差。
+    """
     if len(s) < LIMIT:
         print(WARNING)
         time.sleep(SLOW)
@@ -52,13 +79,18 @@ def main():
                 return
 
             strategy_picked = None
+            # 策略映射
             strategies = {'1': allUniqueSet, '2': allUniqueSort}
+            # 选择策略
             while strategy_picked not in strategies.keys():
                 strategy_picked = input(STRAT_IN_DESC)
 
                 try:
+                    # 从策略映射中获取策略
                     strategy = strategies[strategy_picked]
+                    # 执行策略
                     result = allUnique(word, strategy)
+                    # 打印结果
                     print(f'allUnique({word}): {result}')
                 except KeyError as err:
                     print(f'Incorrect option: {strategy_picked}')
